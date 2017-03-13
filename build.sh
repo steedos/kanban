d8142c4f071aaebb1369c13a5c5b67d9b57f912b
Jack Zhuang <zhuangjianguo@me.com>
2017-03-13 13:29:43 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+meteor npm install
+meteor build --server https://cn.steedos.com/board --directory /srv/wekan
+cd /srv/wekan/bundle/programs/server
+rm -rf node_modules
+rm -f npm-shrinkwrap.json
+npm install --registry https://registry.npm.taobao.org -d
+
+cd /srv/wekan/
+pm2 restart wekan.0
